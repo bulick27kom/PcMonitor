e690d4f536fbc7f67a0332d20644a374b9d2bce1
Roman Brauer <bulick27kom@gmail.com>
2025-02-06 14:30:08 +0200

    add swager

--- PcMonitorWebApi/Program.cs
+++ PcMonitorWebApi/Program.cs
@@ -1,47 +1,45 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.OpenApi.Models;
+
 internal class Program
 {
     private static void Main(string[] args)
     {
         WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
-        // Add services to the container.
-        // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-        builder.Services.AddOpenApi();
+        // Добавление сервисов в контейнер
+        builder.Services.AddControllers();
+
+        // Добавление поддержки Swagger/OpenAPI
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen(options =>
+        {
+            options.SwaggerDoc("v1", new OpenApiInfo
+            {
+                Title = "PC Monitor API",
+                Version = "v1"
+            });
+        });
 
         var app = builder.Build();
 
-        // Configure the HTTP request pipeline.
+        // Настройка HTTP конвейера запросов
         if (app.Environment.IsDevelopment())
         {
-            app.MapOpenApi();
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "PC Monitor API V1");
+            });
         }
 
         app.UseHttpsRedirection();
 
-        var summaries = new[]
-        {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-        app.MapGet("/weatherforecast", () =>
-        {
-            var forecast = Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                (
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]
-                ))
-                .ToArray();
-            return forecast;
-        })
-        .WithName("GetWeatherForecast");
+        // Настройка маршрутов
+        app.MapControllers();
 
         app.Run();
     }
 }
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
+
